d155261f58158735698598d04283eee09bdf6c4b
Rian23-R4 <ryan23nugraha23@gmail.com>
2020-12-19 23:22:56 +0700

    first commit

--- index.js
+++ index.js
@@ -1,34 +1,14 @@
-// const webdriver = require('selenium-webdriver');
-const express = require('express');
-const  app = express();
-
+//import package
 var PORT = process.env.PORT || 3000;
-// var By = webdriver.By;
+//server
+const express = require('express');
+const app = express();
 
 app.get('/', (req,res) => {
 	res.send('BySonics Home Base Server');
 });
 
-// app.get('/', function (req, res) {
-//     var driver = new webdriver.Builder()
-//         .forBrowser('phantomjs')
-//         .build();
-//     driver.get('http://www.google.com/ncr');
-//     driver.findElement(By.name('q')).sendKeys('webdriver');
-//     driver.findElement(By.name('btnG')).click();
-//     driver.wait(function() {
-//         return driver.getTitle().then(function(title) {
-//             console.log(title);
-//             return title === 'webdriver - Google Search';
-//         });
-//     }, 5000).then(function() {
-//         res.status(200).send('Done');
-//     }, function(error) {
-//         res.status(200).send(error);
-//     });
-//     driver.quit();
-// });
-
+//start LISTEN AT PORT:
 app.listen(PORT, function () {
 	console.log('Server running');
 });
