7af72cc12e4dad614466076a85bc33c5dd2f22b7
Rian23-R4 <ryan23nugraha23@gmail.com>
2020-12-20 00:36:08 +0700

    first commit

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
 //import package
 var PORT = process.env.PORT || 3000;
 //server
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const {Builder, By, Key, until} = require('selenium-webdriver');
-const webdriver = require('selenium-webdriver');
-const chrome = require('selenium-webdriver/chrome');
-const firefox = require('selenium-webdriver/firefox');
+const { Builder, By, Key, until } = require("selenium-webdriver");
+const webdriver = require("selenium-webdriver");
+const chrome = require("selenium-webdriver/chrome");
+const firefox = require("selenium-webdriver/firefox");
 
-let driver = new webdriver.Builder()
-    .forBrowser('firefox')
-    .build();
-
-
-app.get('/', (req,res) => {
-    try {
-        driver.get('http://www.google.com/ncr');
-    } finally {
-        driver.quit();
-    }    
+app.get("/", (req, res) => {
+  let driver = new webdriver.Builder().forBrowser("chrome").build();
+  try {
+    driver.get("http://www.google.com/ncr");
+  } finally {
+    driver.quit();
+  }
 });
 
 //start LISTEN AT PORT:
 app.listen(PORT, function () {
-	console.log('Server running');
+  console.log("Server running");
 });
