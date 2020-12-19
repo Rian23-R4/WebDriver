776b2d9990a19ff16f4d787b53cbb955bb54e497
Rian23-R4 <ryan23nugraha23@gmail.com>
2020-12-19 23:52:56 +0700

    first commit

--- index.js
+++ index.js
@@ -5,25 +5,25 @@ const express = require('express');
 const app = express();
 
 var webdriver = require('selenium-webdriver');
-var By = webdriver.By;
+// var By = webdriver.By;
 
 app.get('/', (req,res) => {
 	var driver = new webdriver.Builder()
         .forBrowser('phantomjs')
         .build();
-    driver.get('http://www.google.com/ncr');
-    driver.findElement(By.name('q')).sendKeys('webdriver');
-    driver.findElement(By.name('btnG')).click();
-    driver.wait(function() {
-        return driver.getTitle().then(function(title) {
-            console.log(title);
-            return title === 'webdriver - Google Search';
-        });
-    }, 5000).then(function() {
-        res.status(200).send('Done');
-    }, function(error) {
-        res.status(200).send(error);
-    });
+    driver.get('http://www.google.com/');
+    // driver.findElement(By.name('q')).sendKeys('webdriver');
+    // driver.findElement(By.name('btnG')).click();
+    // driver.wait(function() {
+    //     return driver.getTitle().then(function(title) {
+    //         console.log(title);
+    //         return title === 'webdriver - Google Search';
+    //     });
+    // }, 5000).then(function() {
+    //     res.status(200).send('Done');
+    // }, function(error) {
+    //     res.status(200).send(error);
+    // });
     driver.quit();
 });
 
