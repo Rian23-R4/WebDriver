07d1e33775b69f95080e700bb9acc965e336c40f
Rian23-R4 <ryan23nugraha23@gmail.com>
2020-12-20 18:19:28 +0700

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,108 @@
 var PORT = process.env.PORT || 3000;
+
 const express = require("express");
 const path = require("path");
 const webdriver = require("selenium-webdriver");
-
+var Base64 = {
+  _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+  encode: function (e) {
+    var t = "";
+    var n, r, i, s, o, u, a;
+    var f = 0;
+    e = Base64._utf8_encode(e);
+    while (f < e.length) {
+      n = e.charCodeAt(f++);
+      r = e.charCodeAt(f++);
+      i = e.charCodeAt(f++);
+      s = n >> 2;
+      o = ((n & 3) << 4) | (r >> 4);
+      u = ((r & 15) << 2) | (i >> 6);
+      a = i & 63;
+      if (isNaN(r)) {
+        u = a = 64;
+      } else if (isNaN(i)) {
+        a = 64;
+      }
+      t =
+        t +
+        this._keyStr.charAt(s) +
+        this._keyStr.charAt(o) +
+        this._keyStr.charAt(u) +
+        this._keyStr.charAt(a);
+    }
+    return t;
+  },
+  decode: function (e) {
+    var t = "";
+    var n, r, i;
+    var s, o, u, a;
+    var f = 0;
+    e = e.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+    while (f < e.length) {
+      s = this._keyStr.indexOf(e.charAt(f++));
+      o = this._keyStr.indexOf(e.charAt(f++));
+      u = this._keyStr.indexOf(e.charAt(f++));
+      a = this._keyStr.indexOf(e.charAt(f++));
+      n = (s << 2) | (o >> 4);
+      r = ((o & 15) << 4) | (u >> 2);
+      i = ((u & 3) << 6) | a;
+      t = t + String.fromCharCode(n);
+      if (u != 64) {
+        t = t + String.fromCharCode(r);
+      }
+      if (a != 64) {
+        t = t + String.fromCharCode(i);
+      }
+    }
+    t = Base64._utf8_decode(t);
+    return t;
+  },
+  _utf8_encode: function (e) {
+    e = e.replace(/\r\n/g, "\n");
+    var t = "";
+    for (var n = 0; n < e.length; n++) {
+      var r = e.charCodeAt(n);
+      if (r < 128) {
+        t += String.fromCharCode(r);
+      } else if (r > 127 && r < 2048) {
+        t += String.fromCharCode((r >> 6) | 192);
+        t += String.fromCharCode((r & 63) | 128);
+      } else {
+        t += String.fromCharCode((r >> 12) | 224);
+        t += String.fromCharCode(((r >> 6) & 63) | 128);
+        t += String.fromCharCode((r & 63) | 128);
+      }
+    }
+    return t;
+  },
+  _utf8_decode: function (e) {
+    var t = "";
+    var n = 0;
+    var r = (c1 = c2 = 0);
+    while (n < e.length) {
+      r = e.charCodeAt(n);
+      if (r < 128) {
+        t += String.fromCharCode(r);
+        n++;
+      } else if (r > 191 && r < 224) {
+        c2 = e.charCodeAt(n + 1);
+        t += String.fromCharCode(((r & 31) << 6) | (c2 & 63));
+        n += 2;
+      } else {
+        c2 = e.charCodeAt(n + 1);
+        c3 = e.charCodeAt(n + 2);
+        t += String.fromCharCode(
+          ((r & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63)
+        );
+        n += 3;
+      }
+    }
+    return t;
+  },
+};
 var app = express();
 chrome = require("selenium-webdriver/chrome");
+var sleep = require("sleep-promise");
 let options = new chrome.Options();
 options.setChromeBinaryPath(process.env.CHROME_BINARY_PATH);
 let serviceBuilder = new chrome.ServiceBuilder(process.env.CHROME_DRIVER_PATH);
@@ -15,25 +113,64 @@ options.addArguments(
   "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36"
 );
 
-app.get("/", async function (req, res) {
-  res.send("BySonics Home Base Server");
-
+app.get("/url/:data", async function (req, res) {
+  let text = Base64.decode(req.params.data);
   let driver = new webdriver.Builder()
     .forBrowser("chrome")
     .setChromeOptions(options)
     .setChromeService(serviceBuilder)
     .build();
-  // await driver.get("http://www.google.com/");
-  // await driver.getPageSource().then(function (source) {
-  //   res.send(source);
-  // });
-  // //await driver.getTitle().then(function(title) { res.send(title); });
-  // console.log("Selesai.");
+  await driver.get(text);
+  await driver.getPageSource().then(function (source) {
+    res.send(source);
+  });
+  //await driver.getTitle().then(function(title) { res.send(title); });
+  console.log("istek geldi.");
 });
 
-app.listen(PORT, function () {
-  console.log("Server running");
+var server = app.listen(8888, function () {
+  console.log("sunucu calisiyor");
 });
+
+//
+//
+//
+
+// const express = require("express");
+// const path = require("path");
+// const webdriver = require("selenium-webdriver");
+
+// var app = express();
+// chrome = require("selenium-webdriver/chrome");
+// let options = new chrome.Options();
+// options.setChromeBinaryPath(process.env.CHROME_BINARY_PATH);
+// let serviceBuilder = new chrome.ServiceBuilder(process.env.CHROME_DRIVER_PATH);
+// options.addArguments("--headless");
+// options.addArguments("--disable-gpu");
+// options.addArguments("--no-sandbox");
+// options.addArguments(
+//   "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36"
+// );
+
+// app.get("/", async function (req, res) {
+//   res.send("BySonics Home Base Server");
+
+//   let driver = new webdriver.Builder()
+//     .forBrowser("chrome")
+//     .setChromeOptions(options)
+//     .setChromeService(serviceBuilder)
+//     .build();
+//   // await driver.get("http://www.google.com/");
+//   // await driver.getPageSource().then(function (source) {
+//   //   res.send(source);
+//   // });
+//   // //await driver.getTitle().then(function(title) { res.send(title); });
+//   // console.log("Selesai.");
+// });
+
+// app.listen(PORT, function () {
+//   console.log("Server running");
+// });
 
 //
 //
