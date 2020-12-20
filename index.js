b5c13b064fcfe28f69c86ac08af9c90237786cfe
Rian23-R4 <ryan23nugraha23@gmail.com>
2020-12-20 19:02:38 +0700

    first commit

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ var PORT = process.env.PORT || 3000;
 const express = require("express");
 var app = express();
 
+const fs = require("fs");
+
 app.get("/", async function (req, res) {
   res.send("Hello Wolrd");
+  res.writeHead(200, { "content-type": "text/html" });
+  fs.createReadStream("index.html").pipe(res);
 });
 
 app.listen(PORT, function () {
